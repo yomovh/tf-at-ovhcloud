c7833755a70fe36fd7eb8c1a94175e3c0aaec699
Guillaume Allee <guillaume.allee@ovhcloud.com>
2023-01-16 15:50:29 +0100

    initial commit with s3_bucket_only example

---
+++ s3_bucket_only/s3_bucket_only.tf
@@ -0,0 +1,109 @@
+########################################################################################
+# This script creates an S3 bucket along with 1 S3 user
+#
+# It requires the following variables to be defined for the OVH provider :
+# OVH_ENDPOINT
+# OVH_APPLICATION_KEY
+# OVH_APPLICATION_SECRET
+# OVH_CONSUMER_KEY
+# The following is required specifically for this script:
+# TF_VAR_OVH_PUBLIC_CLOUD_PROJECT_ID that shall be filled with your public cloud project id or it will be requested on script startup
+########################################################################################
+
+
+########################################################################################
+#     Variables
+########################################################################################
+variable ovh_public_cloud_project_id{
+  type = string
+}
+
+variable region {
+  type        = string
+  default     = "gra"
+}
+
+variable s3_endpoint {
+  type        = string
+  default     = "https://s3.gra.io.cloud.ovh.net"
+}
+
+variable user_desc_prefix {
+  type        = string
+  default     = "[TF] User created by s3 terraform script"
+}
+
+variable bucket_name {
+   type       = string
+   default    ="tf-s3-bucket-only"
+}
+
+
+#######################################################################################
+#     Providers
+########################################################################################
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+
+    ovh = {
+      source  = "ovh/ovh"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+    region      = var.region
+    access_key  = ovh_cloud_project_user_s3_credential.s3_admin_cred.access_key_id
+    secret_key  = ovh_cloud_project_user_s3_credential.s3_admin_cred.secret_access_key
+
+    #OVH implementation has no STS service
+    skip_credentials_validation = true
+    skip_requesting_account_id  = true
+    # the gra region is unknown to AWS hence skipping is needed.
+    skip_region_validation = true
+    endpoints {
+        s3 = var.s3_endpoint
+    }
+}
+
+
+########################################################################################
+#     User / Credential
+########################################################################################
+resource "ovh_cloud_project_user" "s3_admin_user" {
+  service_name = var.ovh_public_cloud_project_id
+  description = "${var.user_desc_prefix} that is used to create S3 access key"
+  role_name = "objectstore_operator"
+} 
+resource "ovh_cloud_project_user_s3_credential" "s3_admin_cred"{
+  service_name = var.ovh_public_cloud_project_id
+  user_id = ovh_cloud_project_user.s3_admin_user.id
+}
+
+########################################################################################
+#     Bucket
+########################################################################################
+resource "aws_s3_bucket" "b"{
+  bucket = "${var.ovh_public_cloud_project_id}-${var.bucket_name}"
+}
+
+
+########################################################################################
+#     Output
+########################################################################################
+output "access_key" {
+  description = "the access key that have been created by the terraform script"
+  value = ovh_cloud_project_user_s3_credential.s3_admin_cred.access_key_id
+}
+ 
+output "secret_key" {
+  description = "the secret key that have been created by the terraform script"
+  value = ovh_cloud_project_user_s3_credential.s3_admin_cred.secret_access_key
+  sensitive = true
+}
