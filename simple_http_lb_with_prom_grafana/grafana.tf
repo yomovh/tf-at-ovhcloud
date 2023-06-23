7d107f29afafe36256366a0745073686adf8b551
Guillaume Allee <guillaume.allee@ovhcloud.com>
2023-06-23 15:23:01 +0200

    format

--- simple_http_lb_with_prom_grafana/grafana.tf
+++ simple_http_lb_with_prom_grafana/grafana.tf
@@ -3,16 +3,16 @@
 ########################################################################################
 resource "ovh_cloud_project_database" "grafana" {
   service_name = var.ovh_public_cloud_project_id
-  description   = "${var.resource_prefix}grafana"
-  engine        = "grafana"
-  version       = "9.1"
-  plan          = "essential"
-  
-  flavor        = "db1-4"
+  description  = "${var.resource_prefix}grafana"
+  engine       = "grafana"
+  version      = "9.1"
+  plan         = "essential"
+
+  flavor = "db1-4"
   nodes {
-    region = substr(var.openstack_region, 0, 3)
+    region     = substr(var.openstack_region, 0, 3)
     network_id = openstack_networking_network_v2.tf_lb_network.id
-    subnet_id = openstack_networking_subnet_v2.tf_lb_subnet.id
+    subnet_id  = openstack_networking_subnet_v2.tf_lb_subnet.id
   }
 }
 resource "ovh_cloud_project_database_ip_restriction" "iprestriction" {
@@ -23,7 +23,7 @@ resource "ovh_cloud_project_database_ip_restriction" "iprestriction" {
 }
 
 data "dns_a_record_set" "grafana" {
-  host=ovh_cloud_project_database.grafana.endpoints[0].domain
+  host = ovh_cloud_project_database.grafana.endpoints[0].domain
   depends_on = [
     ovh_cloud_project_database.grafana
   ]
@@ -32,10 +32,10 @@ data "dns_a_record_set" "grafana" {
 resource "grafana_data_source" "prometheus" {
   type = "prometheus"
   name = "prom"
-  url = "http://${openstack_compute_instance_v2.prometheus.access_ip_v4}:9090"
+  url  = "http://${openstack_compute_instance_v2.prometheus.access_ip_v4}:9090"
 }
 
-resource "grafana_dashboard" "octavia_dashboard"{
+resource "grafana_dashboard" "octavia_dashboard" {
   config_json = file("resources/octavia-amphora-load-balancer_rev1.json")
 }
 
@@ -43,6 +43,6 @@ resource "ovh_cloud_project_database_user" "avnadmin" {
   service_name = var.ovh_public_cloud_project_id
   engine       = ovh_cloud_project_database.grafana.engine
   cluster_id   = ovh_cloud_project_database.grafana.id
-  name = "avnadmin"
+  name         = "avnadmin"
 }
 
